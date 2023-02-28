293bd100c025e7461c5ab16c091d70359b0a0d5d
m-agahi <64974579+m-agahi@users.noreply.github.com>
2023-02-28 18:41:20 +0100

    day1- part 2 is still not producing the correct results according to website

---
+++ 2022/05/code.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		fmt.Println("fucking error man")
+		panic(e)
+	}
+}
+
+func main() {
+	filePath := os.Args[1]
+	readFile, err := os.Open(filePath)
+	check(err)
+
+	var word string
+	var linenumber1, linenumber2 int = 0, 0
+	var numberofstacks int
+	// var sum1 int
+	// var sum2 int
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		if fileScanner.Text() != "" {
+			word = fileScanner.Text()
+			if string(word[1]) == "1" {
+				numberofstacks = int(word[len(word)-2]) - 48
+				break
+			}
+			linenumber1++
+		} else {
+			fmt.Println("nothing")
+		}
+	}
+
+	stacks := make([][]string, 0)
+	stacks2 := make([][]string, 0)
+
+	_, err = readFile.Seek(0, 0)
+	check(err)
+	fileScanner = bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	tempstacks := make([][]string, 0)
+	for fileScanner.Scan() {
+		if fileScanner.Text() != "" {
+			word = fileScanner.Text()
+			linenumber2++
+			if linenumber2 > linenumber1 {
+				break
+			}
+			tempstack := make([]string, 0)
+			for h := 0; h < len(word); h++ {
+				tempstack = append(tempstack, string(word[h]))
+			}
+			tempstacks = append(tempstacks, [][]string{tempstack}...)
+		}
+	}
+	for z := 0; z < numberofstacks; z++ {
+		tempstack := make([]string, 0)
+		for j := linenumber1 - 1; j >= 0; j-- {
+			index := (z * 4) + 1
+			boxname := string(tempstacks[j][index])
+			if boxname != " " {
+				tempstack = append(tempstack, boxname)
+			}
+		}
+		stacks = append(stacks, [][]string{tempstack}...)
+		stacks2 = append(stacks2, [][]string{tempstack}...)
+	}
+
+	instrunctions := make([][]int, 0)
+	for fileScanner.Scan() {
+		if fileScanner.Text() != "" {
+			word = fileScanner.Text()
+			temp := make([]int, 0)
+			var command int = 0
+			if string(word[6]) != " " {
+				command = ((int(word[5]) - 48) * 10) + (int(word[6]) - 48)
+				temp = append(temp, command)
+				command = int(word[13]) - 48
+				temp = append(temp, command)
+				command = int(word[18]) - 48
+				temp = append(temp, command)
+			} else {
+				command = int(word[5]) - 48
+				temp = append(temp, command)
+				command = int(word[12]) - 48
+				temp = append(temp, command)
+				command = int(word[17]) - 48
+				temp = append(temp, command)
+			}
+			instrunctions = append(instrunctions, [][]int{temp}...)
+		}
+	}
+
+	for k := 0; k < len(instrunctions); k++ {
+		for pop := 0; pop < instrunctions[k][0]; pop++ {
+			from := instrunctions[k][1] - 1
+			to := instrunctions[k][2] - 1
+			object := stacks[from][len(stacks[from])-1]
+			stacks[from] = stacks[from][:len(stacks[from])-1]
+			stacks[to] = append(stacks[to], object)
+		}
+	}
+	fmt.Println("part 1 results")
+	for helia := 0; helia < numberofstacks; helia++ {
+		fmt.Printf("%s", stacks[helia][len(stacks[helia])-1])
+	}
+	fmt.Println("")
+
+	fmt.Println(stacks2)
+	// fmt.Println(instrunctions)
+	for k := 0; k < len(instrunctions); k++ {
+		from := instrunctions[k][1] - 1
+		to := instrunctions[k][2] - 1
+
+		fmt.Println("instructions:", instrunctions[k])
+		fmt.Println("from:", stacks2[from])
+		fmt.Println("to:  ", stacks2[to])
+
+		object := stacks2[from][(len(stacks2[from]) - instrunctions[k][0]):]
+		fmt.Println("what:", object)
+
+		for o := 0; o < instrunctions[k][0]; o++ {
+			stacks2[to] = append(stacks2[to], string(object[o]))
+		}
+
+		stacks2[from] = stacks2[from][:(len(stacks2[from]) - instrunctions[k][0])]
+
+		fmt.Println("from:", stacks2[from])
+		fmt.Println("to:  ", stacks2[to])
+		fmt.Println("")
+
+	}
+	fmt.Println(stacks2)
+	fmt.Println("part 2 results")
+	for dana := 0; dana < numberofstacks; dana++ {
+		fmt.Printf("%s", stacks2[dana][len(stacks2[dana])-1])
+	}
+	fmt.Println("")
+	// fmt.Printf("numberofstacks = %d\nline number1 = %d\nline number2 = %d\n", numberofstacks, linenumber1, linenumber2)
+	// fmt.Printf("part one answer is = %d\npart two answer is = %d\n", sum1, sum2)
+
+	readFile.Close()
+
+}
