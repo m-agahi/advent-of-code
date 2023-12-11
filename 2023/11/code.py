c38d70caed6c52f90b7473c7130324e9c0efeca2
Max Agahi <max.agahi@quinyx.com>
2023-12-11 13:35:06 +0100

    i am

--- 2023/11/code.py
+++ 2023/11/code.py
@@ -20,19 +20,13 @@ def readinput():
 def main():
     dataset = readinput()
     dataset = convert_input(dataset)
-    # dataset = convert_input(input)
+    
     ans = part1(dataset)
-    # for line in dataset: 
-    #     print(line)
-    #     for char in line:
-    #         print(char,end='')
-    #     print("")
     print(f"part 1: {ans}")
-    # ans = part2()
-    # print(f"part 2: {ans}")
 
-
-    return
+    ans = part2(dataset)
+    print(f"part 2: {ans}")
+    return 
 
 ############################################################
 def convert_input(dataset):
@@ -40,20 +34,33 @@ def convert_input(dataset):
     for line in dataset:
         temp=[]
         for i in range(len(line)):
-            temp.append(line[i])
+            char = line[i]
+            if char == ".":
+                temp.append(1)
+            else:
+                temp.append(-1)
         result.append(temp)
     return result
 
 ############################################################
 def part1(dataset):
+    expanded = []
     hs = []
     vs = []
-    expanded = []
+    hs,vs = detect_empty(dataset)
+    expanded = expand(dataset,hs,vs,2)
+    sum_distancd= calculate(expanded)
+    return sum_distancd
+
+############################################################
+def detect_empty(dataset):
+    hs = []
+    vs = []
     # rows
     for i in range(len(dataset)):
         empty = True
         for char in dataset[i]:
-            if char == "#": 
+            if char == -1: 
                 empty = False
                 break
         hs.append(empty)
@@ -62,30 +69,30 @@ def part1(dataset):
         empty = True
         for i in range(len(dataset)):
             char = dataset[i][j]
-            if char == "#":
+            if char == -1:
                 empty = False
                 break
         vs.append(empty)
-    expanded = expand(dataset,hs,vs)
-    sum_distancd= calculate(expanded)
-    return sum_distancd
+    return hs,vs
 
 ############################################################
-def expand (dataset,hs,vs):
+def expand (dataset,hs,vs,oldness):
     expanded = []
     empty_row = []
     cols = 0
     for col in vs:
         if col: cols +=1
     for i in range(len(hs)):
-        empty_row.append(".")
+        empty_row.append(oldness)
+
     temp = []
     for i in range(len(hs)):
         if not hs[i]:
             temp.append(dataset[i])
         else:
             temp.append(empty_row)
-            temp.append(dataset[i])
+            # temp.append(dataset[i])
+
     for i in range(len(temp)):
         t = []
         for j in range(len(temp[0])):
@@ -93,37 +100,58 @@ def expand (dataset,hs,vs):
             if not vs[j]:
                 t.append(char)
             else:
-                t.append(".")
-                t.append(char)
+                t.append(oldness)
+                # t.append(char)
         expanded.append(t)
+    # for line in expanded: print(line)
     return expanded
 
 ############################################################
 def calculate(dataset):
     summary = 0
     galaxies = []
-    for i in range(len(dataset)):
-        for j in range(len(dataset[0])): 
-            char = dataset[i][j]
-            if char == "#":
-                galaxies.append([i,j])
+    for row in range(len(dataset)):
+        for col in range(len(dataset[0])): 
+            char = dataset[row][col]
+            if char == -1:
+                galaxies.append([row,col])
     for i in range(len(galaxies)-1):
         for j in range(i+1,(len(galaxies))):
-            g1x = galaxies[i][0] 
-            g1y = galaxies[i][1] 
-            g2x = galaxies[j][0] 
-            g2y = galaxies[j][1] 
+            g1r = galaxies[i][0] 
+            g1c = galaxies[i][1] 
+            g2r = galaxies[j][0] 
+            g2c = galaxies[j][1] 
             
-            distance = abs(g2x-g1x) + abs(g2y-g1y)
+            sumr = 0
+            rowadd = 1
+            if g1r == len(dataset): rowadd = 0
+            for row in range(g1r+rowadd,g2r+1):
+                sumr += abs(dataset[row][g1c])
+
+            sumc = 0
+            cols = g1c
+            cole = g2c
+            if g1c > g2c : 
+                cols = g2c
+                cole = g1c
+            coladd = 1
+            if cols == len(dataset[0]): coladd = 0
+            for col in range(cols+coladd,cole+1):
+                sumc += abs(dataset[g1r][col])
+            distance = sumc+sumr
             # print(galaxies[i],galaxies[j],distance)
             summary += distance    
-    # print(summary)
     return summary
 
 ############################################################
-def part2():
-    
-    return 
-
+def part2(dataset):
+    expanded = []
+    hs = []
+    vs = []
+    hs,vs = detect_empty(dataset)
+    expanded = expand(dataset,hs,vs,1000000)
+    sum_distancd= calculate(expanded)
+    for line in expanded: print(line)
+    return sum_distancd
 ############################################################
 main()
